b4e6129adcf7aecc0024caeeb13eee56e49e547f
UmarEngineer <ohojimirzayev@gmail.com>
2022-12-10 21:42:02 +0500

    Correct inline handler function

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from aiohttp import ClientSession
 import time
 import wikipedia
 from config import API_TOKEN
+from pprint import pprint
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,6 +18,34 @@ wikipedia.set_lang('uz')
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.answer(f"Assalomu aleykum, {message.from_user.full_name}!\n\nQidirayotgan maqolangiz uchun kalit so'zlarni kiriting yuboring yoki @wikipedio_bot so'zidan so'ng kalit so'zni yozing")    
+
+
+@dp.inline_handler()
+async def empty_query(query: types.InlineQuery):    
+    time.sleep(1)
+    if not query.query == "":
+        try:
+            que = wikipedia.search(query.query.lower(), results=10)
+            resultSearch = []
+            for qu in que:
+                resultSearch.append(
+                    types.InlineQueryResultArticle(
+                        id=qu,
+                        title=qu,
+                        input_message_content=types.InputTextMessageContent(
+                            message_text=qu
+                        ),
+                        thumb_url="https://cdn.icon-icons.com/icons2/2845/PNG/512/wikipedia_logo_icon_181367.png",
+                        thumb_width=80,
+                        thumb_height=80,
+                    )
+                )
+            await query.answer(
+                resultSearch
+            )
+            # await State.inlineQuery.set()
+        except wikipedia.exceptions.DisambiguationError as e:
+            pprint(e.options)
 
 
 
@@ -32,9 +61,8 @@ async def sendWiki(message: types.Message):
     try:
         respond = wikipedia.page(message.text)
         respond2 = wikipedia.summary(message.text)
-        global respond_url
-        respond_url = respond.url
         more = f"\n\nBatafsil o'qish uchun bosing\n{respond.url}"
+
         await bot.send_photo(chat_id=message.from_user.id, photo=respond.images[0])
         if len(respond2) > 4096:
             for x in range(0, len(respond2), 4096):
@@ -58,33 +86,6 @@ async def sendWiki(message: types.Message):
         logging.info(e)
         await message.answer(f" Hurmatli <b>{message.from_user.full_name}</b> bot bilan bog'liq xato yuz berdi, qayta urunib ko'ring. \n\nYoki bu mavzuga oid o'zbek tilida maqola topilmadi.\nUshbu mavzuga oid maqola yozib O'zbek tilidagi Wikipedia rivojiga o'z hissangizni qo'sha olasiz. Shu bilan birga WikiMarafonda qatnashib pul yutug'lariga ham ega bo'lishingiz mumkin. WikiStipendiya haqida ba'tafsil ma'lumot olish uchun quyidagi manzilni ziyorat qiling: https://t.me/wikistipendiya \n\n \"Wikipedia siz kabi insonlar tomonidan yaratildi\"")
         # await message.answer(f"Hurmatli {message.from_user.full_name} afsuski, bu mavzu bo'yicha maqola topilmadi")
-
-async def empty_query(query: types.InlineQuery):
-    
-    time.sleep(1)
-    if not query.query == " ":
-        try:
-            que = wikipedia.search(query.query.lower(), results=3)
-            resultSearch = []
-            for qu in que:
-                resultSearch.append(
-                    types.InlineQueryResultArticle(
-                        id=qu,
-                        title=qu,
-                        input_message_content=types.InputTextMessageContent(
-                            message_text=qu
-                        ),
-                
-                    )
-                )
-            await query.answer(
-                resultSearch
-            )
-            # await State.inlineQuery.set()
-        except wikipedia.exceptions.DisambiguationError as e:
-            pprint(e.options)
-
-
     
 
 if __name__ == '__main__':
